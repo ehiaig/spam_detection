539a412dafe3b9898f08a09e6fd99614e881c87a
Ehi <ehiagheaigg@gmail.com>
2019-02-16 23:55:35 +0100

    Few Updates

--- app.py
+++ app.py
@@ -15,24 +15,24 @@ def predict():
 
 	df= pd.read_csv("spam.csv", encoding="latin-1")
 	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-	# Features and Labels
+	
 	df['label'] = df['v1'].map({'ham': 0, 'spam': 1})
 	df['message'] = df['v2']
 	X = df['message']
 	y = df['label']
 	
 	# Extract Feature With CountVectorizer
-	cv = CountVectorizer()
-	X = cv.fit_transform(X) # Fit the Data
+	bow_transformer = CountVectorizer()
+	X = bow_transformer.fit_transform(df['message'])
 
-	#Load the save model
-	spam_model = open('spam_model.pkl','rb')
+	#Load the model that was created initially
+	spam_model = open('our_model.pkl','rb')
 	clf = joblib.load(spam_model)
 
 	if request.method == 'POST':
 		message = request.form['sample_text']
 		data = [message]
-		vect = cv.transform(data).toarray()
+		vect = bow_transformer.transform(data).toarray()
 		my_prediction = clf.predict(vect)
 
 		my_prediction = str(my_prediction[0])
